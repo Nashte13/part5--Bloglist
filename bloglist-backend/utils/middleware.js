3091d55c9786652ee63ef958a8708f47de590a60
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-03-23 16:01:14 +0300

    debugging and using express

--- bloglist-backend/utils/middleware.js
+++ bloglist-backend/utils/middleware.js
@@ -1,6 +1,7 @@
 const logger = require('./logger')
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
+const app = express()
 const cors = require('cors')
 
 app.use(cors())
