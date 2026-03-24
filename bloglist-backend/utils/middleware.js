6be8dc888f68a9f3df90080f07d631e9e15c3e77
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-03-24 14:35:53 +0300

    correctifying express error

--- bloglist-backend/utils/middleware.js
+++ bloglist-backend/utils/middleware.js
@@ -1,6 +1,7 @@
 const logger = require('./logger')
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
+const express = require('express')
 const app = express()
 const cors = require('cors')
 
