76fe771689ab8d8ba2ef2b2126a6b62158c34eb3
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-04-15 14:43:16 +0300

    making display updates

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -109,9 +109,12 @@ const App = () => {
 
           {blogForm()}
 
-          {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} />
-          ))}
+          <div>
+            {blogs.map((blog) => (
+              <Blog key={blog.id} blog={blog} />
+            ))}
+            
+          </div>
         </div>
       )}
     </div>
