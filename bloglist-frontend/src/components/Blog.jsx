b4dfc57236cefaef118252476126fb739f9bfa30
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-04-16 13:28:13 +0300

    adding button to show details

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,22 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} by {blog.author}
-  </div>  
-)
+import {useState} from 'react'
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+  return (
+    <div>
+      {blog.title} by {blog.author}
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? "hide" : "view"}
+      </button>
+
+      {showDetails && (
+        <div>
+          <p>URL: {blog.url}</p>
+          <p>Likes: {blog.likes}</p>
+          <p>Author: {blog.author}</p>
+        </div>
+      )}
+    </div>
+  ); 
+}
 
 export default Blog
