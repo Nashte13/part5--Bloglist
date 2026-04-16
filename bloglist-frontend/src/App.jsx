6956b5a1204a4440e5c75ee6312356a461be5f69
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-04-16 13:39:31 +0300

    passing update likes function

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -123,7 +123,7 @@ const App = () => {
 
           <div>
             {blogs.map((blog) => (
-              <Blog key={blog.id} blog={blog} />
+              <Blog key={blog.id} blog={blog} updateLikes={updateLikes} />
             ))}
             
           </div>
