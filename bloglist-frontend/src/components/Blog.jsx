6658730cab7a89c2dc88fb6c4dfbbd2544dd0798
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-04-17 14:14:47 +0300

    formatting code

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,39 +1,43 @@
-import {useState} from 'react'
-import blogs from '../services/blogs'
+import { useState } from "react";
+import blogs from "../services/blogs";
 const Blog = ({ blog, updateLikes }) => {
-  const [showDetails, setShowDetails] = useState(false)
+  const [showDetails, setShowDetails] = useState(false);
 
   const handleLike = () => {
-    const updatedBlog = { ...blog, likes: blogs.likes + 1, user: blog.user.id || blog.user }
-    updateLikes(blog.id, updatedBlog)
-  }
+    const updatedBlog = {
+      ...blog,
+      likes: blogs.likes + 1,
+      user: blog.user.id || blog.user,
+    };
+    updateLikes(blog.id, updatedBlog);
+  };
 
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
-    border: 'solid',
+    border: "solid",
     borderWidth: 1,
-    marginBottom: 5
-  }
+    marginBottom: 5,
+  };
   return (
     <div style={blogStyle}>
       {blog.title} by {blog.author}
       <button onClick={() => setShowDetails(!showDetails)}>
         {showDetails ? "hide" : "view"}
       </button>
-
       {showDetails && (
         <div>
           <p>URL: {blog.url}</p>
           <p>
-            Likes: {blog.likes}{""}
+            Likes: {blog.likes}
+            {""}
             <button onClick={handleLike}>like</button>
           </p>
           <p>Author: {blog.author}</p>
         </div>
       )}
     </div>
-  ); 
-}
+  );
+};
 
-export default Blog
+export default Blog;
