22554338e35875669f58717330ba0ea9e0ad8bc2
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-04-27 13:11:32 +0300

    button test

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -1,24 +1,43 @@
-import { render, screen } form '@testing-library/react'
-import Blog from './Blog'
-import {vi} from 'vitest'
+import { render, screen } from "@testing-library/react";
+import Blog from "./Blog";
+import { expect, vi, test } from "vitest";
+import userEvent from "@testing-library/user-event";
 
-test('renders title and author but not url and likes by default', () => {
-    const blog = {
-        title: 'Test Blog',
-        author: 'Nash',
-        url: 'http://example.com',
-        likes: 5,
-        user: {id: '123'}
-    }
+test("renders   title and author but not url and likes by default", () => {
+  const blog = {
+    title: "Test Blog",
+    author: "Nash",
+    url: "http://example.com",
+    likes: 5,
+    user: { id: "123" },
+  };
 
+  render(<Blog blog={blog} updateLikes={vi.fn()} deleteBlog={vi.fn()} />);
 
-    render(<Blog blog={blog} updateLikes={vi.fn()} deleteBlog={vi.fn()} />)
+  expect(screen.getByText("Testing React Component by Nash")).toBDefined();
 
-    expect(screen.getByText('Testing React Component by Nash')).toBDefined()
+  const detail = screen.queryByText("http://example.com");
+  const likes = screen.queryByText("Likes: 5");
 
-    const detail = screen.queryByText('http://example.com')
-    const likes = screen.queryByText('Likes: 5')
+  expect(detail).toBeNull();
+  expect(likes).toBeNull();
+});
 
-    expect(detail).toBeNull()
-    expect(likes).toBeNull()
-})
+test("shows url and likes when view button s clicked", async () => {
+  const blog = {
+    title: "Test Blog",
+    author: "Nash",
+    url: "http://example.com",
+    likes: 5,
+    user: { id: "123" },
+  };
+
+  render(<Blog blog={blog} updateLikes={vi.fn()} deleteBlog={vi.fn()} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText("View");
+  await user.click(button);
+
+  expect(screen.getByText("http://example.com")).toBDefined();
+  expect(screen.getByText("Likes: 5")).toBDefined();
+});
