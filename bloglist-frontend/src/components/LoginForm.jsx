d19e04ed2d9c35320a4e843dc68a46390189ee51
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-06-03 13:08:32 +0300

    making imports

--- bloglist-frontend/src/components/LoginForm.jsx
+++ bloglist-frontend/src/components/LoginForm.jsx
@@ -1,32 +1,37 @@
-const LoginForm = ({handleSubmit, username, handleUsernameChange, password, handlePasswordChange}) => {
+import {TextField, Button, Paper, Typography, Box} from '@mui/material';
+const LoginForm = ({ handleSubmit, username, handleUsernameChange, password, handlePasswordChange }) => {
     return (
-         <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            username
-            <input
-              type="text"
-              value={username}
-              onChange={handleUsernameChange}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            password
-            <input
-              type="text"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-          </label>
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
-    )
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <Input >
+              <label>
+                username
+                <input
+                  type="text"
+                  value={username}
+                  onChange={handleUsernameChange}
+                />
+              </label>
+            </Input>
+          </div>
+          <div>
+            <label>
+              password
+              <input
+                type="text"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </label>
+          </div>
+          <Button variant="contained" color="primary" type="submit">
+            Login
+          </Button>
+        </form>
+      </div>
+    );
 }
 
 export default LoginForm;
