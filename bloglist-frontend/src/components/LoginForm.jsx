4da591338ef4cef1101f24a675329efafb72690a
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-06-03 13:44:58 +0300

    styling login form

--- bloglist-frontend/src/components/LoginForm.jsx
+++ bloglist-frontend/src/components/LoginForm.jsx
@@ -1,35 +1,42 @@
 import {TextField, Button, Paper, Typography, Box} from '@mui/material';
 const LoginForm = ({ handleSubmit, username, handleUsernameChange, password, handlePasswordChange }) => {
-    return (
-      <div>
-        <h2>Login</h2>
+  return (
+    <Paper
+      elevation={3}
+      sx={{ padding: 4, maxWidth: 400, margin: "20px auto" }}
+    >
+      <Typography variant="h5" component="h2" gutterBottom>
+        Login
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+      >
         <form onSubmit={handleSubmit}>
-          <div>
-              <label>
-                username
-                <input
-                  type="text"
-                  value={username}
-                  onChange={handleUsernameChange}
-                />
-              </label>
-          </div>
-          <div>
-            <label>
-              password
-              <input
-                type="text"
-                value={password}
-                onChange={handlePasswordChange}
-              />
-            </label>
-          </div>
+          <TextField
+            label="Username"
+            variant="outlined"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+
+          <TextField
+            label="Password"
+            type="password"
+            variant="outlined"
+            value={password}
+            onChange={handlePasswordChange}
+            fullWidth
+            required
+          />
           <Button variant="contained" color="primary" type="submit">
             Login
           </Button>
         </form>
-      </div>
-    );
+      </Box>
+    </Paper>
+  );
 }
 
 export default LoginForm;
