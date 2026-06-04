1bd2b5a4213316a7b393df65eb0178de91cb7cd4
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-06-04 11:27:43 +0300

    removing icons impost

--- bloglist-frontend/src/components/BlogDetail.jsx
+++ bloglist-frontend/src/components/BlogDetail.jsx
@@ -1,7 +1,6 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { Card, CardContent, CardActions, Button, Box, Typography } from "@mui/material";
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import DeleteIcon from '@mui/icons-material/Delete';
+
 
 const BlogDetail = ({ blogs, updateLikes, deleteBlog, currentUser }) => {
   const { id } = useParams();
